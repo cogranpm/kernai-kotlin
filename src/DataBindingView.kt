be8eb88a07348dfae6695a6e6d606184b9c61d20
paul martin <mussym@live.com>
2020-07-24 15:45:57 -0500

    trying to add a view

---
+++ src/DataBindingView.kt
@@ -0,0 +1,9 @@
+import org.eclipse.swt.SWT
+import org.eclipse.swt.widgets.Composite
+import org.eclipse.swt.widgets.Control
+import org.eclipse.swt.widgets.Label
+
+
+fun makeView(parent: Composite) {
+   Composite(parent, SWT.NONE)
+}
